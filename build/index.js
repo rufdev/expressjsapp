2cc7632b4de0966a878f6b648450dde2aad2f94f
RUFINO JOHN <aguilarufino@gamil.com>
2022-03-08 13:55:55 +0800

    add build folder

---
+++ build/index.js
@@ -0,0 +1,1 @@
+(()=>{var e={860:e=>{"use strict";e.exports=require("express")}},r={};function t(o){var s=r[o];if(void 0!==s)return s.exports;var n=r[o]={exports:{}};return e[o](n,n.exports,t),n.exports}(()=>{const e=t(860)();e.get("/",((e,r)=>{r.send("Hello IT 3rd year!")})),e.listen(3e3,(()=>{console.log("Example app listening on port 3000")}))})()})();
